8bac0d9ce907254337c268e8bf559fb95743bc08
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-14 18:14:36 +0530

    [Surajmal Kumar].Adding UC2.adding peak and pop functionality to Stack using Linked list.

--- Stack.java
+++ Stack.java
@@ -1,7 +1,13 @@
 package day13linkedlist.day13linkedlistproblem;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jdk.internal.org.jline.utils.Log;
+
 public class Stack<I> {
 	LinkedList<I> linkedList;
+	private static final Logger log=LogManager.getLogger(Stack.class);
 
 	public Stack() {
 		this.linkedList = new LinkedList<I>();
@@ -13,7 +19,14 @@ public class Stack<I> {
 	public void printingStack() {
 		linkedList.printingList();
 	}
-
+	public InterfaceNode<I> peakFinder(){
+		return linkedList.headPart;
+	}
+	
+	public InterfaceNode<I> pop(){
+		return linkedList.pop();
+	}
+	
 	public static void main(String[] args) {
 		Stack<Integer> stackFormation = new Stack<Integer>();
 		Node<Integer> firstElement = new Node<Integer>(56);
@@ -23,5 +36,11 @@ public class Stack<I> {
 		stackFormation.push(secondElement);
 		stackFormation.push(firstElement );
 		stackFormation.printingStack();
+		while(stackFormation!=null) {
+			
+			log.debug("Peak : " + stackFormation.peakFinder());
+			log.debug("Pop : " + stackFormation.pop());	
+			
+		}
 	}
 }
